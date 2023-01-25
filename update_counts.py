a3da74e7e0c9b42b12ae8961c9dfd4c66689132f
Karol Kosek <krkk@serenityos.org>
2023-01-25 21:10:51 +0100

    Plot bullet labels on the left side and add colors

--- update_counts.py
+++ update_counts.py
@@ -157,9 +157,9 @@ def write_graphs(most_recent_commit):
         '' using 1:3 lw 1 title "Core::File", \
         '' using 1:4 lw 1 title "AK::Stream", \
         '' using 1:5 lw 1 title "C FILE*", \
-        '< tail -n 1 tagged_history.csv' using 1:2:2 with labels point pointtype 7 offset 1,char -.75 notitle, \
-        '< tail -n 1 tagged_history.csv' using 1:3:3 with labels point pointtype 7 offset 1,char 0.5 notitle, \
-        '< tail -n 1 tagged_history.csv' using 1:4:4 with labels point pointtype 7 offset 1,char 0.5 notitle
+        '< tail -n 1 tagged_history.csv' using 1:2:2 with labels right point linecolor 1 pointtype 7 offset -2,-.5 notitle, \
+        '< tail -n 1 tagged_history.csv' using 1:3:3 with labels right point linecolor 2 pointtype 7 offset -2,0 notitle, \
+        '< tail -n 1 tagged_history.csv' using 1:4:4 with labels right point linecolor 3 pointtype 7 offset -2,0 notitle
     """
 
     # that's a pretty awful delta cuz it still works per commit rather than per day/whatever but eeeeeehhhhhhh it's good enough for me
